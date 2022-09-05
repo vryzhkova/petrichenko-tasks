2a44d12d34626311da95df2c9dea1dce9b41e470
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-09-05 16:01:57 +0400

    added task 2

--- practice-2/js/script.js
+++ practice-2/js/script.js
@@ -14,51 +14,118 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener("DOMContentLoaded", () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    // 1)
+    
+    const adv = document.querySelectorAll('.promo__adv img');
+    const genre = document.querySelector('.promo__genre');
+    const poster = document.querySelector('.promo__bg');
+    const movieList = document.querySelector('.promo__interactive-list');
 
-// 1)
+    const deleteAdv = (arr) => {
+        adv.forEach(item => {
+            item.remove();
+        });
+    };
 
-const adv = document.querySelectorAll('.promo__adv img');
+    deleteAdv(adv);
 
-adv.forEach(item => {
-    item.remove();
-});
+    // 2), 3)
 
-// 2)
+    const makeChanges = () => {
+        genre.textContent = 'драма';
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    }
 
-const promoGenre = document.querySelector('.promo__genre');
-promoGenre.textContent = 'драма';
+    makeChanges();
+    
+    // 4), 5)
 
-// 3)
+    const sortArr = (arr) => {
+        arr.sort();
+    }
 
-const promoBg = document.querySelector('.promo__bg');
-promoBg.style.backgroundImage = 'url("img/bg.jpg")';
+    sortArr(movieDB.movies);
 
+/* Task2 - Задания на урок:
 
-// 4)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-const filmsList = document.querySelector('.promo__interactive-list');
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-filmsList.innerHTML = '';
-movieDB.movies.sort();
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-movieDB.movies.forEach((film, i) => {
-    filmsList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-             <div class="delete"></div>
-        </li>
-    `;
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+// 1), 5)
+const form = document.querySelector('.add');
+const formInput = document.querySelector('.adding__input');
+const checkbox = document.querySelector('[type="checkbox"]');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let newFilm = formInput.value;
+    const favorite = checkbox.checked;
+
+    if (newFilm) {
+
+        // 2)
+        if(newFilm.length > 21) {
+            newFilm = `${newFilm.substring(0, 22)}...`
+        }
+
+        if(favorite) {
+            console.log('Добавляем любимый фильм');
+        }
+
+        movieDB.movies.push(newFilm);
+    
+        createMovieList(movieDB.movies, movieList);
+    }
+
+    event.target.reset();
 })
 
-// 5)
+function createMovieList(films, parent) {
+    parent.innerHTML = '';
+    sortArr(films);
+    
+    films.forEach((film, i) => {
+        parent.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+                 <div class="delete"></div>
+            </li>
+        `;
+    });
 
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
 
+            createMovieList(films, parent);
+        })
+    })
+}
+
+createMovieList(movieDB.movies, movieList);
+});
 
